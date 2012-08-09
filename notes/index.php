d09b9f40838b0297a83d8268c3142d40c6f73810
Jacques Berger <jacquesberger@hotmail.com>
2012-08-09 09:16:16 -0400

    Mettre les presentations dans files

--- notes/index.php
+++ notes/index.php
@@ -1,4 +1,19 @@
 <?php
+/* Copyright 2011 Jacques Berger
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 function createXPathExplorer($fileName) {
 	$xml = new DOMDocument();
 	$xml->Load($fileName);
@@ -29,11 +44,11 @@ function displayLectureNotes($students, $Id) {
 				echo "<li>";
 				echo $note->getElementsByTagName("label")->item(0)->textContent;
 				echo " (";
-				echo '<a href="' . $note->getElementsByTagName("pdf")->item(0)->textContent . '">';
+				echo '<a href="files/' . $note->getElementsByTagName("pdf")->item(0)->textContent . '">';
 				echo "PDF";
 				echo "</a>";
 				echo " | ";
-				echo '<a href="' . $note->getElementsByTagName("impress")->item(0)->textContent . '">';
+				echo '<a href="files/' . $note->getElementsByTagName("impress")->item(0)->textContent . '">';
 				echo "Impress";
 				echo "</a>";
 				echo ")";
